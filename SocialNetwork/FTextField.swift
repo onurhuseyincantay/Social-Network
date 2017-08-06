671e1b5c1ddc4af7f66f22ff5eeed36dc1b77e2c
onur hüseyin çantay <onurhuseyincantay@onurs-MacBook-Pro.local>
2017-08-06 15:05:01 +0300

    Email Authentication and Keyboard Done button Handled

--- SocialNetwork/FTextField.swift
+++ SocialNetwork/FTextField.swift
@@ -10,12 +10,19 @@ import UIKit
 
 class FTextField: UITextField {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.borderColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.2).cgColor
+        layer.borderWidth = 1.0
+        layer.cornerRadius = 2.0
+        
     }
-    */
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 10, dy: 5)
+    }
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 10, dy: 5)
+    }
 
 }
--- SocialNetwork/SıgnInVC.swift
+++ SocialNetwork/SıgnInVC.swift
@@ -7,19 +7,73 @@
 //
 
 import UIKit
-
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 class SignInVC: UIViewController {
+    @IBOutlet weak var txtEmailAddress: FTextField!
+    @IBOutlet weak var txtPassword: FTextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func KeyboardHiding(_ sender: FTextField) {
+        sender.resignFirstResponder()
+    }
+    
+
+    @IBAction func fbButtonClicked(_ sender: AnyObject) {
+        let facebookLogin = FBSDKLoginManager()
+        facebookLogin.logIn(withReadPermissions: ["email"] , from: self){ (result,error) in
+            if error != nil {
+                print("Onur: Unable to Authenticate with Facebook- \(error.debugDescription)")
+            }else if result?.isCancelled == true{
+                print("Onur: User Canceled the Facebook Authentication " )
+            }else{
+                print("Onur: Succesfully authenticated to Facebook")
+                let credential = FacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        }
     }
 
+    @IBAction func btnSignInClicked(_ sender: Any) {
+        if let email = txtEmailAddress.text , let pass = txtPassword.text {
+            Firebase.Auth.auth().signIn(withEmail: email, password: pass, completion: {(user , error) in
+                if error == nil {
+                    print("Onur :  Email authenticated with Firebase")
+                }else{
+                    Firebase.Auth.auth().createUser(withEmail: email, password: pass, completion:{(user,error) in
+                        if error != nil {
+                            print("Onur : Unable to Authenticated with Firebase")
+                        }else{
+                            
+                            print("Onur : Succesfully Authenticated with Firebase")
+                        }
+                    })
+                }
+        })
+        }
+    }
+
+    func firebaseAuth(_ credential : AuthCredential) {
+        Firebase.Auth.auth().signIn(with: credential, completion: { (user , error ) in
+            if error != nil {
+                print("Onur: Unable to Authenticate with Firebase :( -\(error.debugDescription)")
+            }else{
+                print("Onur : Succesfully Authenticated with Firebase ")
+            }
+        })
+    
+    
+    
+
+
+
+
+    }
 
 }
 
