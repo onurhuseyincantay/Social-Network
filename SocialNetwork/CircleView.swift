e28bb480f179daf563060fa3f151eebd5170b0cd
onur hüseyin çantay <onurhuseyincantay@onurs-MacBook-Pro.local>
2017-08-07 15:38:29 +0300

    Feed UI first part segue designed

--- SocialNetwork/CircleView.swift
+++ SocialNetwork/CircleView.swift
@@ -1,22 +1,26 @@
 //
-//  FView.swift
+//  CircleView.swift
 //  SocialNetwork
 //
-//  Created by onur hüseyin çantay on 04/08/2017.
+//  Created by onur hüseyin çantay on 07/08/2017.
 //  Copyright © 2017 onur hüseyin çantay. All rights reserved.
 //
 
 import UIKit
 
-class FView: UIView {
-
+class CircleView: UIImageView {
     override func awakeFromNib() {
         super.awakeFromNib()
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.shadowRadius = 2.0 
+        
+    }
+
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+           layer.cornerRadius = self.frame.width/2
     }
 
 }
